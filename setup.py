318a9b72fec7377dab2b2580ca72de5b7b1644ae
Mikhael Korneev <korneevm@gmail.com>
2022-02-24 13:27:40 +0300

    BANG-927: python 3.10

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
